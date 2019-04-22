430019d4a51c0e738e479a7f1f60b66a2d2e6400
mxrafal <maya.rafalowicz@gmail.com>
2019-04-22 09:48:03 -0700

    Replace zodiac sign with selected sign

--- home.py
+++ home.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from mrmodel import get_model_api
 from textgenrnn import textgenrnn  
 import os
+import re
 
 
 model = get_model_api()
@@ -29,23 +30,22 @@ def contact():
 
 @app.route("/predict")
 def predict():
-    selected_sign = request.args.get('type')
+    #selected_sign = request.args.get('type')
+    #print(selected_sign)
+    selected_sign = request.args.get('sign')
+    #print(selected_sign)
     # res = mrmodel.generate()
     res = model()
     signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
-    words = res.split(' ')
-    for word in words:
-        for s in signs:
-            if word == s:
-                word = selected_sign
-    res = ' '.join(words)    
-    # return render_template("zodiac_predict.html", )
+    for sign in signs:
+        res = res.replace(sign, selected_sign)
     return render_template("zodiac_predict.html", horoscope=res)
 
 if __name__ == "__main__":
-    if os.environ['ENV'] == 'production':
-        app.debug = True
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+    if 'ENV' in os.environ:
+        if os.environ['ENV'] == 'production':
+            app.debug = True
+            port = int(os.environ.get("PORT", 5000))
+            app.run(host='0.0.0.0', port=port)
     else:
         app.run(debug=True, threaded=False)
